61b51f624b77df73b973941f079af269ac0839b7
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-07-23 14:41:25 -0600

    Added functions.

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 const express = require("express");
 const path = require("path");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+const = reservationInfo = [
+    {
+        routeName: "reservation",
+        name: custName,
+        phone: custPhone,
+        email: custEmail,
+        id: custID
+    },
+]
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.post("/api/orders", function(req,res) {
+    let newOrder = req.body;
+
+
+
+
+})
